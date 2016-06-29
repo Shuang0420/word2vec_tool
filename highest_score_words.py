263fdaad268a91ab37a1855c77aec2d6bda61b40
Shuang0420 <sxu1@andrew.cmu.edu>
2016-06-29 11:41:17 +0800

    sim

---
+++ highest_score_words.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import gensim
+from gensim.models import Word2Vec
+import os
+import logging
+import re
+import multiprocessing
+import argparse
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# logging information
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+logging.root.setLevel(level=logging.INFO)
+
+def simWords(modelname):
+    global resultsList
+    wordsList=generateWords(modelname)
+    resultsList=dict()
+    for word in wordsList:
+        results = model.most_similar(word.decode('utf8'))
+        resultsList[word]=results[0]
+    l = sorted(resultsList.items(), key=lambda cosList: -cosList[1][1])
+    flag = True
+    for i in range(0,1000):
+        if flag:
+            print '{}\t{}\t{}'.format(l[i][0], l[i][1][0],l[i][1][1])
+            flag=False
+        else:
+            flag=True
+        #for i in results:
+        #    print '{}\t{}'.format(i[0], i[1])
+    simWordsSingle()
+
+
+
+def simWordsSingle():
+    while (1):
+        w = raw_input('enter the word ')
+        word = []
+        for i in w.split():
+            i = i.decode('utf8')
+            word.append(i)
+        results = model.most_similar(word)
+        for i in results:
+            print '{}\t{}'.format(i[0], i[1])
+
+def generateWords(modelname):
+    wordsList=[]
+    filename = modelname + '.vector'
+    f = open(filename,'r')
+    for line in f:
+        word=line.split(' ',1)[0]
+        if len(word)>=6:
+            wordsList.append(word)
+    return wordsList
+
+if __name__ == '__main__':
+    # load model
+    modelname = raw_input('enter the model you want to check ')
+    model = Word2Vec.load(modelname)
+    simWords(modelname)
